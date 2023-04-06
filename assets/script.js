2f96bb370a39f0dc55f38acaea634734cfdb9f89
Noah Hoang <hoincbusiness@gmail.com>
2023-04-06 16:48:36 -0700

    began index.js eventlistener click, changed header color to mimic sunrise in relation to work-day-planner

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  $(".planner").on("click", ".btn", function (e) {});
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
